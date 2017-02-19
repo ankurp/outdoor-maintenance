54083c7ba92003f58dbd9e31410b22a9ae78c59b
Ankur Patel <ankur.patel@ymail.com>
2017-02-19 16:24:14 -0500

    Updating migrations

--- db/migrate/20170219194846_create_recommendations.rb
+++ db/migrate/20170219194846_create_recommendations.rb
@@ -2,9 +2,11 @@ class CreateRecommendations < ActiveRecord::Migration[5.0]
   def change
     create_table :recommendations do |t|
       t.belongs_to :user, foreign_key: true
-      t.belongs_to :recommender, foreign_key: true
+      t.belongs_to :recommender, index: true
 
       t.timestamps
     end
+
+    add_foreign_key :recommendations, :users, column: :recommender_id
   end
 end
